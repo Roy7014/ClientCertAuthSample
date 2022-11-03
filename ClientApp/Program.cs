6b3c81eabc95ca625b37a3da6e9f9c6487e77e33
Roy7014 <sohamroy@microsoft.com>
2022-11-03 17:29:19 -0400

    Client App

---
+++ ClientApp/Program.cs
@@ -0,0 +1,83 @@
+﻿// using System.Net.Http;
+// using System.Security.Authentication;
+// using System.Security.Cryptography.X509Certificates;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Security.Authentication;
+using ClientApp;
+
+AuthJwt ob1 = new AuthJwt();
+var token = ob1.generatejwt(10);
+
+X509Certificate2 clientCert = certval.GetClientCertificate();
+
+
+var handler = new HttpClientHandler();
+handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+handler.SslProtocols = SslProtocols.Tls12;
+//var clientCert = new X509Certificate2("C:\\Certificates\\client.pfx","Pa$$w0rd");
+//var cert= X509Certificate certificate = GetCert("ClientCertificate.cer");
+//var thb = cert.Thumbprint;
+Console.WriteLine(clientCert.Thumbprint);
+handler.ClientCertificates.Add(clientCert);
+//Console.WriteLine(thb);
+//var client = new HttpClient();
+var client = new HttpClient(handler);
+//client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+
+var result = client.GetStringAsync("https://restapi20220930115015.azurewebsites.net/WeatherForecast").GetAwaiter().GetResult();
+Console.WriteLine(result);
+Console.ReadKey();
+
+
+
+
+
+
+//namespace ClientApp
+//{
+//    internal class Program
+//    {
+//        static async Task Main(string[] args)
+//        {
+//            await SendRequest();
+//            Console.ReadKey();
+//        }
+
+//        static async Task SendRequest()
+//        {
+
+//            try
+//            {
+//                var cert = new X509Certificate2("C:\\Certificates\\client.pfx", "Pa$$w0rd");
+//                var handler = new HttpClientHandler();
+//                handler.ClientCertificates.Add(cert);
+//                var client = new HttpClient(handler);
+
+//                var request = new HttpRequestMessage()
+//                {
+//                    RequestUri = new Uri("https://localhost:443/home/privacy"),
+//                    Method = HttpMethod.Get,
+//                };
+
+//                var response = await client.SendAsync(request);
+
+//                Console.WriteLine(response.StatusCode);
+//                if (response.IsSuccessStatusCode)
+//                {
+//                    var responseContent = await response.Content.ReadAsStringAsync();
+//                    Console.WriteLine(responseContent);
+//                }
+//            }
+
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.ToString());
+//            }
+//        }
+//    }
+//}
---
+++ ClientApp/certval.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    internal class certval
+    {
+        public static X509Certificate2 GetClientCertificate()
+        {
+            X509Store userCaStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            try
+            {
+                userCaStore.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection certificatesInStore = userCaStore.Certificates;
+                X509Certificate2Collection findResult = certificatesInStore.Find(X509FindType.FindBySerialNumber, "48012820387b4045efe6137e8f000201282038", true);
+                X509Certificate2 clientCertificate = null;
+                if (findResult.Count == 1)
+                {
+                    clientCertificate = findResult[0];
+                }
+                else
+                {
+                    throw new Exception("Unable to locate the correct client certificate.");
+                }
+                return clientCertificate;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                userCaStore.Close();
+            }
+        }
+    }
+}
